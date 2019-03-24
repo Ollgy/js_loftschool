e538982c53b7920c200015b5178b65d7c9394fd3
Olga Baranova <olga.baranova@uchi.ru>
2019-03-24 19:27:48 +0300

    details src

--- src/scripts/reviewObj.js
+++
@@ -1,8 +0,0 @@
-export default class ReviewObj {
-    constructor (parent, coords, pos, data) {
-        this.coords = coords;
-        this.address = parent.querySelector('.header__place').innerText;
-        this.pos = pos;
-        this.reviews = data;
-    }
-}
